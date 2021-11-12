fa0ddf2f500f65c4028671ec21db2a7f1b73531b
Austin Huang <gengminghuang@gmail.com>
2021-11-12 12:21:57 +0900

    Thumbnail upload working... not yet formatted

--- src/Video.js
+++ src/Video.js
@@ -22,6 +22,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './Video.css'
 
 import Three from './Three'
+
 const server_url =
   process.env.NODE_ENV === 'production'
     ? 'https://video.sebastienbiollo.com'
@@ -38,6 +39,101 @@ var socket = null
 var socketId = null
 var elms = 0
 
+const LE = true // Binary GLTF is little endian.
+const MAGIC_glTF = 0x676c5446
+const GLB_FILE_HEADER_SIZE = 12
+const GLB_CHUNK_LENGTH_SIZE = 4
+const GLB_CHUNK_TYPE_SIZE = 4
+const GLB_CHUNK_HEADER_SIZE = GLB_CHUNK_LENGTH_SIZE + GLB_CHUNK_TYPE_SIZE
+const GLB_CHUNK_TYPE_JSON = 0x4e4f534a
+const GLB_CHUNK_TYPE_BIN = 0x004e4942
+
+function getMagic(dataView) {
+  const offset = 0
+  return dataView.getUint32(offset)
+}
+
+function getVersion(dataView) {
+  const offset = 4
+  let version = dataView.getUint32(offset, LE)
+  return version
+}
+
+function getTotalLength(dataView) {
+  const offset = 8
+  let length = dataView.getUint32(offset, LE)
+  return length
+}
+
+function getGLBMeta(dataView) {
+  let magic = getMagic(dataView)
+  let version = getVersion(dataView)
+  let total = getTotalLength(dataView)
+
+  return {
+    magic: magic,
+    version: version,
+    total: total,
+  }
+}
+
+function getJsonData(dataView) {
+  const offset = GLB_FILE_HEADER_SIZE
+
+  let chunkLength = dataView.getUint32(offset, LE)
+  console.log('ChunkLen ' + chunkLength)
+
+  let chunkType = dataView.getUint32(offset + GLB_CHUNK_LENGTH_SIZE, LE)
+  console.log('ChunkType ' + chunkType.toString(16))
+
+  if (chunkType !== GLB_CHUNK_TYPE_JSON) {
+    console.warn("This GLB file doesn't have a JSON part.")
+    return
+  }
+
+  const jsonChunk = new Uint8Array(
+    dataView.buffer,
+    offset + GLB_CHUNK_HEADER_SIZE,
+    chunkLength
+  )
+  const decoder = new TextDecoder('utf8')
+  const jsonText = decoder.decode(jsonChunk)
+  const json = JSON.parse(jsonText)
+  console.log(json)
+
+  return {
+    json: json,
+    length: chunkLength,
+  }
+}
+
+function getThumbnail(jsonData, buffer, offset) {
+  let index = -1
+  let mimeType = ''
+
+  for (var i = 0; i < jsonData.json.images.length; i++) {
+    if (jsonData.json.images[i].name === 'Thumbnail') {
+      index = jsonData.json.images[i].bufferView
+      mimeType = jsonData.json.images[i].mimeType
+      break
+    }
+  }
+
+  if (index === -1) {
+    console.warn('Thumbnail field was not found.')
+    return
+  }
+
+  const view = jsonData.json.bufferViews[index]
+  let imgBuf = new Uint8Array(
+    buffer,
+    offset + GLB_CHUNK_HEADER_SIZE + view.byteOffset,
+    view.byteLength
+  )
+
+  return URL.createObjectURL(new Blob([imgBuf]))
+}
+
 class Video extends Component {
   constructor(props) {
     super(props)
@@ -49,7 +145,35 @@ class Video extends Component {
 
     this.onDrop = (files) => {
       this.setState({files})
+      let reader = new FileReader();
+      let input = files[0];
+
+      reader.addEventListener('load', (e) => {    
+        let raw = e.target.result
+        let ds = new DataView(raw)
+      
+        let glbMeta = getGLBMeta(ds)
+        console.log('magic ' + glbMeta.magic.toString(16))
+        if (glbMeta.magic !== MAGIC_glTF) {
+          console.warn('This file is not a GLB file.')
+          return
+        }
+        console.log('Version ' + glbMeta.version)
+        console.log('Total Length ' + glbMeta.total)
+      
+        const jsonData = getJsonData(ds)
+        const offset = GLB_FILE_HEADER_SIZE + GLB_CHUNK_HEADER_SIZE + jsonData.length
+        let dataChunkType = ds.getUint32(offset + GLB_CHUNK_LENGTH_SIZE, LE)
+      
+        if (dataChunkType !== GLB_CHUNK_TYPE_BIN) {
+          console.warn("This GLB file doesn't have a binary buffer.")
+          return
+        }
+    
+        this.setState({imagePreviewUrl: getThumbnail(jsonData, ds.buffer, offset)})}, true)
+      reader.readAsArrayBuffer(input)
     };
+
     this.state = {
       video: false,
       audio: false,
@@ -61,7 +185,9 @@ class Video extends Component {
       newmessages: 0,
       askForUsername: true,
       username: faker.internet.userName(),
+      file: '',
       files: [],
+      imagePreviewUrl: '',
     }
     connections = {}
 
@@ -558,6 +684,8 @@ class Video extends Component {
   }
 
   render() {
+    let {imagePreviewUrl} = this.state;
+    console.log(imagePreviewUrl)
     const files = this.state.files.map(file => (
       URL.createObjectURL(file)
     ))
@@ -626,6 +754,7 @@ class Video extends Component {
                   </div>
                   <aside>
                     <h4>Files</h4>
+                    <ul><img src={imagePreviewUrl}/></ul>
                     <ul><Three url={files[0]}/></ul>
                   </aside>
                 </section>
